00001402a923ed78b3dd1afebb56f22027b65962
BAITAP BUOI_02 <dangtrongluong4@gmail.com>
2025-04-13 15:55:17 +0700

    source code Bai8_for

---
+++ Bai8_For/Bai8_for.js
@@ -0,0 +1,11 @@
+console.log("Hello world from JS");
+
+// Khi duyet qua cac phan tu muon lap
+for (let i = 0; i < 10; i++) {
+  console.log("check variable i = ", i);
+}
+
+let arr = ["MU", "ManCity", "Liver", "Chelsea"];
+for (let i = 0; i < arr.length; i++) {
+  console.log("check variable arr = ", arr[i]);
+}
