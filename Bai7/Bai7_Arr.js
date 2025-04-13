2433d08f6866d5f3fcb2f8d2e73a0d8b38026fbe
BAITAP BUOI_02 <dangtrongluong4@gmail.com>
2025-04-13 15:27:40 +0700

    source code Bai7_Arr

---
+++ Bai7/Bai7_Arr.js
@@ -0,0 +1,18 @@
+console.log("Hello world from JS");
+
+//Object Key : Value
+let obj = {
+  name: "Luong",
+  age: 19,
+};
+console.log("Check data obj: ", obj);
+
+let arr = ["MU", "Chelsea", "Liverpool"];
+console.log("Type of arr: ", typeof arr, arr);
+
+// Object inside Arr
+let family = [
+  { name: "Luong", age: 19 },
+  { name: "Duyen", age: 19 },
+];
+console.log("My family: ", family);
